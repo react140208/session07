de33a93587da926e885e6d1d6d7edc76c390d0aa
Pasha <pashaie@gmail.com>
2023-12-26 20:24:07 +0330

    image

--- expoApp/App.js
+++ expoApp/App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState([]);
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    //IIFE
+    (async () => {
+      const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const json = await resp.json();
+      setData(json);
+      console.log(counter);
+    })();
+    return () => {
+      console.log("cleanup");
+    };
+  }, [counter]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Salam Donya 💃 {counter}</Text>
       <Button onPress={() => setCounter(counter + 1)} title="+"></Button>
+      <Text style={styles.title}>{data.length}</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: "https://reactnative.dev/img/tiny_logo.png",
+        }}
+      />
+      {/* <Image style={styles.tinyLogo} source={require("assets/icon.png")} /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -23,5 +45,9 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: "bold",
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
