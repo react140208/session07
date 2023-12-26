1763fa0ffc1e74c71a062941930de3800f189909
Pasha <pashaie@gmail.com>
2023-12-26 19:34:48 +0330

    counter

--- expoApp/App.js
+++ expoApp/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Salam Donya</Text>
+      <Text style={styles.title}>Salam Donya 💃 {counter}</Text>
+      <Button onPress={() => setCounter(counter + 1)} title="+"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
   },
 });
